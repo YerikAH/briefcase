10ac066a71d3ade6ed774aa5b9a604ea5f3ab7ce
Harvey Yerik <yerik05vh@gmail.com>
2022-07-08 16:37:53 -0500

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:yerikah/my-briefcase.git main:gh-pages
+# git push -f git@github.com:yerikah/briefcase.git main:gh-pages
 
 cd -
